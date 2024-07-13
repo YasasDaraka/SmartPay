719ee0dd4099df8f608f83276b38546ee4777f02
YasasDaraka <darakayasas@gmail.com>
2024-07-13 09:13:12 +0530

    update

--- SmartPay/app/_layout.tsx
+++ SmartPay/app/_layout.tsx
@@ -1,9 +1,14 @@
 import { Stack } from "expo-router";
-
+import { Provider as PaperProvider } from 'react-native-paper';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 export default function RootLayout() {
   return (
+    <SafeAreaProvider>
+    <PaperProvider>
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen name="index"  options={{ headerShown: false }} />
     </Stack>
+    </PaperProvider>
+    </SafeAreaProvider>
   );
 }
