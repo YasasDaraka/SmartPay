b30dcb0d2dbc3e62aa5b0f6ad3853bb1b18921eb
YasasDaraka <darakayasas@gmail.com>
2024-07-15 22:37:39 +0530

    update

--- SmartPay/app/screens/pages/FundTransfer.tsx
+++ SmartPay/app/screens/pages/FundTransfer.tsx
@@ -8,6 +8,7 @@ import PagerView from 'react-native-pager-view';
 const back = require('../../../assets/images/back.png');
 const sendmoney = require('../../../assets/images/sendmoney.png');
 const mobile = require('../../../assets/images/mobile.png');
+const arrow = require('../../../assets/images/arrowlight.png');
 
 export default function FundTransfer() {
 
@@ -40,16 +41,16 @@ export default function FundTransfer() {
                         <View className='flex items-center h-full w-full' key="1" >
                             <View className='flex justify-center items-center h-1/5 w-full'>
                                 <Headline className='text-[25px] m-0 p-0 mt-10'>Sending money made</Headline>
-                                <View  className='flex flex-row justify-center'>
-                                <Headline className='m-0 p-0'>easy with </Headline>
-                                <Headline className='text-[26px] font-bold m-0 p-0'>SmartPay</Headline>
+                                <View className='flex flex-row justify-center'>
+                                    <Headline className='m-0 p-0'>easy with </Headline>
+                                    <Headline className='text-[26px] font-bold m-0 p-0'>SmartPay</Headline>
                                 </View>
                             </View>
 
                             <View className='flex flex-col h-4/5 w-full'>
                                 <View className='flex flex-row h-full'>
                                     <View className='h-full w-[45vw] '>
-                                    <Image source={sendmoney} className='h-full w-full'></Image>
+                                        <Image source={sendmoney} className='h-full w-full'></Image>
                                     </View>
                                     <View className='h-full w-3/5 bg-red-800'></View>
                                 </View>
@@ -86,6 +87,10 @@ export default function FundTransfer() {
                             <View className='flex ml-5 justify-center w-[65vw] h-2/3 '>
                                 <Headline className='text-xl font-extrabold relative right-4'>Send Money</Headline>
                             </View>
+
+                            <View className='flex justify-center items-center w-9 h-9 bg-[#403D1C] relative right-9 rounded-md'>
+                                <Image source={arrow} className='w-6 h-6'></Image>
+                            </View>
                         </View>
 
                         <View className='flex-row bg-white items-center justify-start px-4 mt-3 h-2/5 w-[94vw] rounded-xl border-[1px] border-black'>
@@ -96,6 +101,10 @@ export default function FundTransfer() {
 
                             <View className='flex ml-5 justify-center w-[65vw] h-2/3 '>
                                 <Headline className='text-xl font-extrabold right-4'>Request Money</Headline>
+                            </View>
+
+                            <View className='flex justify-center items-center w-9 h-9 bg-[#403D1C] relative right-9 rounded-md'>
+                                <Image source={arrow} className='w-6 h-6'></Image>
                             </View>
                         </View>
 
