e8050af7b1d8b5e97be63cd59a5435df19812e59
YasasDaraka <darakayasas@gmail.com>
2024-07-17 20:53:32 +0530

    update

--- SmartPay/app/screens/pages/Instruments.tsx
+++ SmartPay/app/screens/pages/Instruments.tsx
@@ -5,6 +5,7 @@ import { LinearGradient } from 'expo-linear-gradient';
 import { Headline } from 'react-native-paper';
 import { useRouter } from 'expo-router';
 const back = require('../../../assets/images/back.png');
+const boc = require('../../../assets/images/boc.png');
 
 export default function Instruments() {
     const router = useRouter();
@@ -26,15 +27,27 @@ export default function Instruments() {
                     <View className='h-[76.5vh] w-full'>
                         <ScrollView className='h-full w-full'>
 
-                            <View className='flex justify-center items-center h-[76.5vh] w-full mt-4'>
-
-                                <View className=' flex items-center w-full h-[30vh] mb-4'>
+                            <View className='flex items-center h-[76.5vh] w-full mt-5'>
 
 
 
-
-
+                                <View className='flex flex-row mt-1 justify-center h-[24vw] w-[91vw] border-[0.2px] border-black rounded-lg'>
+                                    <View className='flex justify-center items-center w-2/6 h-full'>
+                                        <Image source={boc} className='w-[18vw] h-[10vw]'></Image>
+                                    </View>
+                                    <View className='flex justify-center w-4/6 h-full'>
+                                        <Headline className='text-base font-extrabold'>myboc</Headline>
+                                        <View className='flex justify-center items-center rounded-md w-16 h-5 bg-[#FCB117]'>
+                                            <Text className='font-semibold'>Savings</Text>
+                                        </View>
+                                        <Headline className='text-base font-extrabold'>XXXX</Headline>
+                                    </View>
                                 </View>
+
+
+
+
+
 
                             </View>
 
@@ -44,7 +57,7 @@ export default function Instruments() {
 
                     <View className='flex flex-row justify-center h-[22vw] w-[91vw] border-[0.2px] border-black rounded-lg'>
                         <View className='flex justify-center items-center w-2/6 h-full'>
-                            <View className='flex justify-center items-center w-[12.5vw] h-[12.5vw] rounded-full border-[0.8px] border-black'></View>
+                            <View className='flex justify-center items-center w-[12.5vw] h-[12.5vw] rounded-full border-[1px] border-black'></View>
                         </View>
                         <View className='flex justify-center w-4/6 h-full'>
                             <Headline className='text-base font-extrabold'>Add new payment Instrument</Headline>
